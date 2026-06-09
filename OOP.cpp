d440a4c594d17735d32d69dcb1aefe27942d46d1
Prince <princecultura10@gmail.com>
2026-06-09 22:18:14 +0800

    Added 1st example (incomplete)

---
+++ OOP.cpp
@@ -0,0 +1,22 @@
+// THIS FILE WILL CONTAIN AN OOP PRACTICES
+#include <iostream>
+using namespace std;
+
+
+class printer{
+    private:
+    string name;
+
+    public: void setName(string n){
+        name = n;
+    }
+    public: string getName(){
+        return name;
+    }
+};
+
+int main(){
+
+
+    return 0;
+}
