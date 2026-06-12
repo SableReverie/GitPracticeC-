32e8e1e864205694488ab4870888fac29ef4e141
Prince <princecultura10@gmail.com>
2026-06-12 20:20:35 +0800

    Added 2 example of threads

--- udemyDSA.cpp
+++ udemyDSA.cpp
@@ -71,4 +71,33 @@ int main() {
 }
 */
 
-// ============================== 
+// ============================== Big O: Drop Non-Dominants ==================================
+
+// So this nested loop is O(n^2) and the other function for-loop is O(n) = O(n^2 + n), but the n will be dropped,
+// so the Big O is still O(n^2).
+/*
+#include <iostream>
+
+using namespace std;
+
+
+void printItems(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << i << j << endl;
+        }
+    }
+
+    for (int k = 0; k < n; k++) {
+            cout << k << endl;
+    }
+}
+
+
+
+int main() { 
+
+    printItems(10);
+
+}
+*/
